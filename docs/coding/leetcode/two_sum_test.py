4e4d1de5dfa6ccd8b244469da6db46a27cdf4629
Xu <i@xuwang.im>
2024-09-09 00:30:10 +0100

    Update nose test pattern

--- docs/coding/leetcode/two_sum_test.py
+++ docs/coding/leetcode/two_sum_test.py
@@ -1,4 +1,5 @@
 from parameterized import parameterized
+import time
 import timeout_decorator
 import unittest
 
@@ -6,6 +7,12 @@ from two_sum import Solution
 
 
 class TestTwoSum(unittest.TestCase):
+    def run(self, result=None):
+        start_time = time.time()
+        super().run(result)
+        end_time = time.time()
+        print(f'{self._testMethodName} ran in {end_time - start_time:.4f} seconds')
+
     @parameterized.expand([
         ([2, 7, 11, 15], 9, [0, 1]),
         ([3, 2, 4], 6, [1, 2]),
